07abe8e7a0d23d7886ead55ff3ee2b7addc77547
Marta Alcalde-Herraiz <marta.alcalde@estudiantat.upc.edu>
2024-04-10 18:47:12 +0100

    tidyData() and first version of plotCompareLargeScale

--- R/plotCompareLargeScaleCharacteristics.R
+++ R/plotCompareLargeScaleCharacteristics.R
@@ -10,20 +10,26 @@ plotCompareLargeScaleCharacteristics <- function(data,
                                                                      #                      strata_level = "X",
                                                                      #                      window_level = "X"),
                                                  facet       = NULL,
-                                                 colorVars   = NULL)
+                                                 colorVars   = NULL,
+                                                 missings    = 0){
 
 
-plotCompareLargeScaleCharacteristics <- function(data,
-                                                 reference,
-                                                 comparator  = NULL,
-                                                 facet       = NULL,
-                                                 colorVars   = NULL) {
-
+  # Check inputs
   checkReferenceAndComparator(reference,  data, type = "reference")
   checkReferenceAndComparator(comparator, data, type = "comparator")
 
+  # Tidying dataset
+  data <- tidyData(data, reference, comparator, missings)
 
-  return(y)
+
+  return( plotfunction(data,
+                       xAxis = "estimate_value",
+                       yAxis = "estimate_value_comparator",
+                       plotStyle = "scatterplot",
+                       facetVarX = NULL,
+                       facetVarY = NULL,
+                       colorVars = NULL,
+                       vertical_x = FALSE))
 }
 
 checkReferenceAndComparator <- function(reference, data, type){
@@ -52,7 +58,7 @@ checkReferenceAndComparator <- function(reference, data, type){
 
       # Check that inputs correspond to tables of the lsc
       x <- names(reference) %in% colnames(data)
-      if(FALSE %in% unique()){
+      if(FALSE %in% x){
         stop(sprintf(paste0(names(reference)[x], " must be a column in large scale characterisation table.\n")))
       }
 
@@ -60,11 +66,84 @@ checkReferenceAndComparator <- function(reference, data, type){
       for(x in names(reference)){
         y <- data |> select(x)
 
-        if(reference[x] %in% (data |> select(x) |> distinct() |> pull())){
+        if(!reference[x] %in% (data |> select(x) |> distinct() |> pull())){
           stop(sprintf(paste0(reference[x], " is not present in column ", x)))
         }
       }
     }
   }
+
+}
+
+tidyData <- function(data, reference, comparator, missings){
+  data <- data |> filter(estimate_type == "percentage",
+                         result_type == "summarised_large_scale_characteristics",
+                         package_name == "PatientProfiles")
+
+  # Create table reference
+  table_reference  <- data |> filter(result_id == 0.5)
+  for(x in names(reference)){
+    table_reference <- table_reference |>
+      union_all(
+        data |>
+          filter(.data[[x]] == reference[[x]])
+      )
+  }
+
+  # Create table comparator
+  if(!is.null(comparator)){
+    table_comparator <- table_reference
+    for(x in names(comparator)){
+      table_comparator <- table_comparator |>
+        union_all(
+          data |>
+            filter(x %in% comparator[x])
+        )
+    }
+  }else{
+    table_comparator <- data |>
+      anti_join(table_reference)
+  }
+
+  # Rename tables
+  table_reference <- table_reference |>
+    rename_at(vars(-c("variable_name", "package_name", "result_type", "package_name",
+                      "package_version", "estimate_name", "estimate_type", all_of(names(comparator)))), ~paste0(.,"_reference"))
+  table_comparator <- table_comparator |>
+    rename_at(vars(-c("variable_name", "package_name", "result_type", "package_name",
+                      "package_version", "estimate_name", "estimate_type", all_of(names(comparator)))), ~paste0(.,"_comparator"))
+
+  # Join both tables
+  data <- table_reference |>
+    full_join(
+      table_comparator,
+      by = c("variable_name", "package_name", "result_type", "package_version", "estimate_name", "estimate_type", all_of(names(comparator)))
+    )
+
+  # Cleaning the dataset
+  data <- data |>
+    mutate(estimate_value_comparator = as.numeric(estimate_value_comparator),
+           estimate_value_reference  = as.numeric(estimate_value_reference)) |>
+    mutate(result_id_reference        = if_else(is.na(result_id_reference),                result_id_comparator,        result_id_reference),
+           cdm_name_reference         = if_else(is.na(cdm_name_reference),                  cdm_name_comparator,         cdm_name_reference),
+           group_name_reference       = if_else(is.na(group_name_reference),              group_name_comparator,       group_name_reference),
+           group_level_reference      = if_else(is.na(group_level_reference),            group_level_comparator,      group_level_reference),
+           strata_name_reference      = if_else(is.na(strata_name_reference),            strata_name_comparator,      strata_name_reference),
+           strata_level_reference     = if_else(is.na(strata_level_reference),          strata_level_comparator,     strata_level_reference),
+           variable_level_reference   = if_else(is.na(variable_level_reference),      variable_level_comparator,   variable_level_reference),
+           additional_name_reference  = if_else(is.na(additional_name_reference),    additional_name_comparator,  additional_name_reference),
+           additional_level_reference = if_else(is.na(additional_level_reference),  additional_level_comparator, additional_level_reference)) |>
+    mutate(result_id_comparator        = if_else(is.na(result_id_comparator),                result_id_reference,        result_id_comparator),
+           cdm_name_comparator         = if_else(is.na(cdm_name_comparator),                  cdm_name_reference,         cdm_name_comparator),
+           group_name_comparator       = if_else(is.na(group_name_comparator),              group_name_reference,       group_name_comparator),
+           group_level_comparator      = if_else(is.na(group_level_comparator),            group_level_reference,      group_level_comparator),
+           strata_name_comparator      = if_else(is.na(strata_name_comparator),            strata_name_reference,      strata_name_comparator),
+           strata_level_comparator     = if_else(is.na(strata_level_comparator),          strata_level_reference,     strata_level_comparator),
+           variable_level_comparator   = if_else(is.na(variable_level_comparator),      variable_level_reference,   variable_level_comparator),
+           additional_name_comparator  = if_else(is.na(additional_name_comparator),    additional_name_reference,  additional_name_comparator),
+           additional_level_comparator = if_else(is.na(additional_level_comparator),  additional_level_reference, additional_level_comparator)) |>
+    tidyr::replace_na(list(estimate_value_comparator = missings, estimate_value_reference = missings)) |>
+    rename(estimate_value = estimate_value_reference)
+
 
 }
