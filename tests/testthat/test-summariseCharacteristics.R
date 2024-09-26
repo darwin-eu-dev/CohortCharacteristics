21c55936582a810d6b71aded2c80033cbbea2ae6
catalamarti <marticatalasabate@gmail.com>
2024-09-26 11:18:54 +0100

    order summariseCharacteristics output
    fix #164

--- tests/testthat/test-summariseCharacteristics.R
+++ tests/testthat/test-summariseCharacteristics.R
@@ -1375,3 +1375,31 @@ test_that("empty input cohort contains name issue #170", {
 
   PatientProfiles::mockDisconnect(cdm = cdm)
 })
+
+test_that("output is always the same", {
+  skip_on_cran()
+  set.seed(123456)
+  cdm <- omock::mockCdmReference() |>
+    omock::mockPerson(nPerson = 100) |>
+    omock::mockObservationPeriod() |>
+    omock::mockConditionOccurrence(recordPerson = 3) |>
+    omock::mockDrugExposure(recordPerson = 4.5) |>
+    omock::mockCohort(
+      numberCohorts = 3, cohortName = c("covid", "tb", "asthma"))
+
+  cdm1 <- CDMConnector::copyCdmTo(
+    con = duckdb::dbConnect(duckdb::duckdb()), cdm = cdm, schema = "main")
+
+  cdm2 <- CDMConnector::copyCdmTo(
+    con = duckdb::dbConnect(duckdb::duckdb()), cdm = cdm, schema = "main")
+
+  result1 <- cdm1$cohort |>
+    CohortCharacteristics::summariseCharacteristics()
+
+  result2 <- cdm2$cohort |>
+    CohortCharacteristics::summariseCharacteristics()
+
+  expect_identical(result1, result2)
+
+  PatientProfiles::mockDisconnect(cdm = cdm)
+})
