9bf3b94fada9b21d3fbafb150de8c823c8d46834
catalamarti <marticatalasabate@gmail.com>
2024-09-26 11:25:21 +0100

    Create test-summariseCohortCount.R
    fix #163

---
+++ tests/testthat/test-summariseCohortCount.R
@@ -0,0 +1,27 @@
+test_that("multiplication works", {
+  skip_on_cran()
+  set.seed(123456)
+  cdm <- omock::mockCdmReference() |>
+    omock::mockPerson(nPerson = 100) |>
+    omock::mockObservationPeriod() |>
+    omock::mockConditionOccurrence(recordPerson = 3) |>
+    omock::mockDrugExposure(recordPerson = 4.5) |>
+    omock::mockCohort(
+      numberCohorts = 3, cohortName = c("covid", "tb", "asthma"))
+
+  cdm1 <- CDMConnector::copyCdmTo(
+    con = duckdb::dbConnect(duckdb::duckdb()), cdm = cdm, schema = "main")
+
+  cdm2 <- CDMConnector::copyCdmTo(
+    con = duckdb::dbConnect(duckdb::duckdb()), cdm = cdm, schema = "main")
+
+  result1 <- cdm1$cohort |>
+    summariseCohortCount()
+
+  result2 <- cdm2$cohort |>
+    summariseCohortCount()
+
+  expect_identical(result1, result2)
+
+  PatientProfiles::mockDisconnect(cdm = cdm)
+})
