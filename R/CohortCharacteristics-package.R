51f8e4ca9fcc0fe13e45ec78827824c84892634e
catalamarti <marticatalasabate@gmail.com>
2024-09-27 13:46:02 +0100

    support lifecycle badges

--- R/CohortCharacteristics-package.R
+++ R/CohortCharacteristics-package.R
@@ -18,10 +18,11 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom rlang %||%
+#' @importFrom dplyr %>%
+#' @importFrom lifecycle deprecated
+#' @importFrom rlang :=
 #' @importFrom rlang .data
 #' @importFrom rlang .env
-#' @importFrom rlang :=
-#' @importFrom dplyr %>%
+#' @importFrom rlang %||%
 ## usethis namespace: end
 NULL
